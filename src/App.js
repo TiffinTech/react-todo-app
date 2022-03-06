9fbcc782f98ada3070d8c8063fb5ee8b6519183c
TiffinTech <harryandmrmugs@gmail.com>
2022-03-06 10:49:20 -0500

    added in bored api

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { useEffect } from "react";
+
 import './App.css';
 
 // components
@@ -9,14 +11,37 @@ import TaskList from './components/TaskList/TaskList';
 import useDataManager from './hooks/useDataManager';
 
 function App() {
-    const {taskList} = useDataManager(); // Instantiating our data manager custom hook
-    
-    return (
-        <div className="App">
-            <Header />
-            <TaskList taskList={taskList} />
-        </div>
-    );
+  const {taskList} = useDataManager(); // Instantiating our data manager custom hook
+  const [dataAPI, setData] = useState(null);
+
+  const fetchData = () => {
+    return fetch('https://www.boredapi.com/api/activity')
+    .then((response) => response.json())
+    .then((dataAPI) => {
+      setData([dataAPI])
+    })
+  }
+
+ useEffect(() => {
+  fetchData()
+ }, [])
+
+
+  return (
+    <div className="App">
+      <Header />
+      <TaskList taskList={taskList} />
+      <h1>THIS API IS CONTROLLING MY LIFE</h1>
+
+        {dataAPI &&
+          dataAPI.map(({ activity }) => (
+            <div key={activity}>
+              <h3>{activity}</h3>
+            </div>
+          ))}
+
+    </div>
+  );
 }
 
 export default App;
