c86808020f8d3fae08c7df24860eb25d879c23f1
Dempsey <101441928+zoobli-io@users.noreply.github.com>
2022-08-25 19:38:00 +0200

    Create Header.js

---
+++ src/components/layout/Header.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import { FaPizzaSlice } from 'react-icons/fa';
+import PropTypes from 'prop-types';
+import { AddTask } from '../AddTask';
+
+export const Header = ({ darkMode, setDarkMode }) => {
+  const [shouldShowMain, setShouldShowMain] = useState(false);
+  const [showQuickAddTask, setShowQuickAddTask] = useState(false);
+
+  return (
+    <header className="header" data-testid="header">
+      <nav>
+        <div className="logo">
+          <img src="/images/logo.png" alt="Todoist" />
+        </div>
+        <div className="settings">
+          <ul>
+            <li className="settings__add">
+              <button
+                data-testid="quick-add-task-action"
+                aria-label="Quick add task"
+                type="button"
+                onClick={() => {
+                  setShowQuickAddTask(true);
+                  setShouldShowMain(true);
+                }}
+              >
+                +
+              </button>
+            </li>
+            <li className="settings__darkmode">
+              <button
+                data-testid="dark-mode-action"
+                aria-label="Darkmode on/off"
+                type="button"
+                onClick={() => setDarkMode(!darkMode)}
+              >
+                <FaPizzaSlice />
+              </button>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+      <AddTask
+        showAddTaskMain={false}
+        shouldShowMain={shouldShowMain}
+        showQuickAddTask={showQuickAddTask}
+        setShowQuickAddTask={setShowQuickAddTask}
+      />
+    </header>
+  );
+};
+
+Header.propTypes = {
+  darkMode: PropTypes.bool.isRequired,
+  setDarkMode: PropTypes.func.isRequired,
+};
