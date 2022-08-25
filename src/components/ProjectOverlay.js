451db6f736c2d72b2785dacc5c6f6e1f4c315a1a
Dempsey <101441928+zoobli-io@users.noreply.github.com>
2022-08-25 22:02:22 +0200

    Create ProjectOverlay.js

---
+++ src/components/ProjectOverlay.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useProjectsValue } from '../context';
+
+export const ProjectOverlay = ({
+  setProject,
+  showProjectOverlay,
+  setShowProjectOverlay,
+}) => {
+  const { projects } = useProjectsValue();
+
+  return (
+    projects &&
+    showProjectOverlay && (
+      <div className="project-overlay" data-testid="project-overlay">
+        <ul className="project-overlay__list">
+          {projects.map((project) => (
+            <li key={project.projectId}>
+              <div
+                data-testid="project-overlay-action"
+                onClick={() => {
+                  setProject(project.projectId);
+                  setShowProjectOverlay(false);
+                }}
+                onKeyDown={(e) => {
+                  if (e.key === 'Enter') {
+                    setProject(project.projectId);
+                    setShowProjectOverlay(false);
+                  }
+                }}
+                role="button"
+                tabIndex={0}
+                aria-label="Select the task project"
+              >
+                {project.name}
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    )
+  );
+};
+
+ProjectOverlay.propTypes = {
+  projects: PropTypes.array,
+};
